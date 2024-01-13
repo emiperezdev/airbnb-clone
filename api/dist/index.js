bbe13b4c96ebfaadfa08968ad952c5b62e4a6872
Emiliano Perez <emiperez.dev@gmail.com>
2024-01-13 14:15:14 -0600

    Set up backend

--- api/dist/index.js
+++ api/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('Hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get('/test', (req, res) => {
+    return res.send('test ok');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => (console.log(`Listening on port ${PORT}...`)));
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world');
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+  return res.send('test ok');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => (console.log(`Listening on port ${PORT}...`)));
