3d3057fd4309ffe8f7b22e38bb0419e866e3077b
Emiliano Perez <emiperez.dev@gmail.com>
2024-01-13 20:40:31 -0600

    Implement bcrypt for passwords

--- api/src/routers/users.ts
+++ api/src/routers/users.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { User, validateUser } from '../models/User';
 import validate from '../middleware/validate';
 import _ from 'lodash';
+import bcrypt from 'bcrypt';
 
 const usersRouter = Router();
 
@@ -10,6 +11,9 @@ usersRouter.post('', validate(validateUser), async (req, res) => {
   if (user) return res.send('User already registered.');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(req.body.password, salt);
 
   await user.validate();
   await user.save();
